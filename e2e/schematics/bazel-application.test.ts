63907cdd8ad6152993fd7cf37033c6b057c6bf07
mrmeku <mrmeku@gmail.com>
2018-03-05 17:30:19 -0700

    E2E test now compiles app with the lib

--- e2e/schematics/bazel-application.test.ts
+++ e2e/schematics/bazel-application.test.ts
@@ -7,10 +7,43 @@ describe('Nrwl Workspace (Bazel)', () => {
         'myApp --directory=myDir',
         '--collection=@nrwl/bazel',
     );
-    newLib('myLib --directory=myDir', '@nrwl/bazel');
-
     checkFilesExist('WORKSPACE', 'BUILD.bazel');
 
+    newLib('myLib --directory=myDir', '@nrwl/bazel');
+    runCommand('bazel build ...');
+
+    updateFile(
+        'apps/my-dir/my-app/src/app/app.module.ts',
+        `import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MyLibModule } from 'proj/libs/my-dir/my-lib/src/my-lib.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, MyLibModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}`);
+
+    updateFile('apps/my-dir/my-app/src/app/BUILD.bazel', `
+package(default_visibility = ["//visibility:public"])
+
+load("@angular//:index.bzl", "ng_module")
+
+ng_module(
+    name = "app",
+    srcs = glob(["*.ts"]),
+    assets = [
+        "app.component.css",
+        "app.component.html",
+    ],
+    deps = [
+      "//libs/my-dir/my-lib/src",
+      "@rxjs",
+    ],
+)
+    `);
     runCommand('bazel build ...');
   }, 1000000);
 });
