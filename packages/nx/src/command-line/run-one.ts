bf52638652a5ecbb9e565e40f0d21cfe91c30b6d
Victor Savkin <mail@vsavkin.com>
2022-05-16 11:24:43 -0400

    fix(core): use absolute paths when resolving package.json files

--- packages/nx/src/command-line/run-one.ts
+++ packages/nx/src/command-line/run-one.ts
@@ -98,6 +98,11 @@ function parseRunOneOptions(
     [project, target, configuration] = splitTarget(
       parsedArgs['project:target:configuration']
     );
+    // this is to account for "nx npmsript:dev"
+    if (project && !target && defaultProjectName) {
+      target = project;
+      project = defaultProjectName;
+    }
   } else {
     target = parsedArgs['project:target:configuration'];
   }
